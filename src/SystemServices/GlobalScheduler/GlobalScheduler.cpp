16c6d7fadb3013963e186c683c6570c2a6f91ac3
Sebion06 <sebastianmoticica@gmail.com>
2022-05-03 02:08:16 +0300

    first commit

---
+++ src/SystemServices/GlobalScheduler/GlobalScheduler.cpp
@@ -0,0 +1,25 @@
+#include "../../MPU6050Driver/MPU6050Driver.h"
+
+void TK_init()
+{
+    MainTaskScheduler();
+}
+
+void TK_1(){
+
+}
+
+void TK_2(){
+    
+}
+
+void TK_3(){
+
+}
+
+
+void MainTaskScheduler(){
+    TK_2();
+}
+
+
---
+++ src/SystemServices/Main/main.cpp
@@ -0,0 +1,57 @@
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+
+long timer = 0;
+
+void printValues(float valueX, float valueY, float valueZ)
+{
+  Serial.print("X: ");
+  if (valueX >= 0) 
+    Serial.print((String)" " + valueX); //(Serial.print(mpu.getGyroX());
+  else
+    Serial.print((String)valueX);
+ 
+  Serial.print("\tY: ");
+  if (valueY >= 0) 
+    Serial.print((String)" " + valueY); //Serial.print(mpu.getGyroY());
+  else
+    Serial.print((String)valueY);
+  Serial.print("\tZ: ");
+
+  if (valueZ >= 0) 
+    Serial.print((String)" " + valueZ); //Serial.print(mpu.getGyroY());
+  else
+    Serial.print((String)valueZ);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  mpu.calcOffsets(true,true); // gyro and accelero
+  Serial.println("Done!\n");
+  
+}
+
+void loop() {
+  mpu.update();
+
+  if(millis() - timer > 100){
+
+    printValues(mpu.getAccX(),mpu.getAccY(),mpu.getAccZ());
+    // printValues(mpu.getGyroX(),mpu.getGyroY(),mpu.getGyroZ());
+    Serial.println("");
+    timer = millis();
+  }
+}
