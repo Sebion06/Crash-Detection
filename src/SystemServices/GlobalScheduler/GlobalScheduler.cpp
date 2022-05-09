25876f01e32fdba5ab9649e0a3ef9fa204605df7
Sebion06 <sebastianmoticica@gmail.com>
2022-05-09 05:31:40 +0300

    fixed tasks sync

--- src/SystemServices/GlobalScheduler/GlobalScheduler.cpp
+++ src/SystemServices/GlobalScheduler/GlobalScheduler.cpp
@@ -1,7 +1,10 @@
 #include "GlobalScheduler.h"
 #include "../../MPU6050Driver/MPU6050Driver.h"
 
-long timer = 0;
+long tk1_timer = 0;
+long tk2_timer = 0;
+long tk3_timer = 0;
+
 void TK_1(){
 
 }
@@ -16,19 +19,19 @@ void TK_3(){
 }
 
 void MainTaskScheduler(){
-    
-    if(micros() - timer > 500){ //TK_2
+    unsigned long current_timer = micros();
+    if(current_timer - tk2_timer > 500){ //TK_2
         TK_2();
-        timer = micros();
+        tk2_timer=current_timer;
     }
-    // if(micros() - timer > 1000){ //TK_3
-    //     Serial.println("test");
-    //     timer = micros();
-    // }
-    // if(micros() - timer > 5000){ //TK_1
-    //     Serial.println("test");
-    //     timer = micros();
-    // }
+    if(current_timer - tk3_timer > 1000){ //TK_3
+        TK_3();
+        tk3_timer=current_timer;
+    }
+    if(current_timer - tk1_timer > 5000){ //TK_1
+        TK_1();
+        tk1_timer=current_timer;
+    }
 }
 
 void TK_init()
--- src/SystemServices/GlobalScheduler/GlobalScheduler.h
+++ src/SystemServices/GlobalScheduler/GlobalScheduler.h
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include <Arduino.h>
 
-extern long timer;
+extern long tk1_timer;
+extern long tk2_timer;
+extern long tk3_timer;
 void TK_1();
 void TK_2();
 void TK_3();
